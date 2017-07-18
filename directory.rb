94c303393455883f11f3c3df86190ec7878d40b9
Will Porter <willjsporter@hotmail.co.uk>
2017-07-18 17:45:07 +0100

    each iteration on student puts

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"]
 
-print "Overall, we have "
-print 11
-puts " great students"
+ "The students of Villains Academy"
+ "-------------"
+ students.each do |student|
+   puts student
+ end
+
+
+print "Overall, we have #{students.length} great students"
