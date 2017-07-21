90f0c8d00f75aa14574122fb1b037ae0f512baca
Will Porter <willjsporter@hotmail.co.uk>
2017-07-21 16:53:45 +0100

    after csv file intro (3.11)

--- directory.rb
+++ directory.rb
@@ -1,80 +1,83 @@
-students_hardcoded = [
-  {name: "Dr. Hannibal Lecter", cohort: :july},
-  {name: "Darth Vader",cohort: :july},
-  {name: "Nurse Ratched",cohort: :july},
-  {name: "Michael Corleone",cohort: :july},
-  {name: "Alex DeLarge",cohort: :july},
-  {name: "The Wicked Witch of the West", cohort: :july},
-  {name: "Terminator",cohort: :july},
-  {name: "Freddy Krueger",cohort: :july},
-  {name: "The Joker", cohort: :july},
-  {name: "Joffrey Baratheon", cohort: :july},
-  {name: "Norman Bates", cohort: :july}
-]
-
+@students=[]
 def input_students
-  puts "Please enter the names, cohort, hobbies and birthday of the students"
-  puts "To finish, just hit return twice upon input of final student data"
-
-require 'date'
-  students=[]
-  stuname=gets.chomp
-    cohort_id=gets.chop
-    hobbyy=gets.gsub(/\n/, '')
-    cohort_id="july" if cohort_id==""
-    bday=Date.parse(gets.chop)
-  while stuname !="" do
-    students.push({name: stuname, cohort: cohort_id.intern, hobbies: hobbyy, birthday: bday})
-    puts "Now we have #{students.length} #{students.length==1 ? "student" : "students"}"
-    stuname=gets.chomp
-    if stuname!=""
-    cohort_id=gets.chomp
-    hobbyy=gets.chomp
-    bday=Date.parse(gets.chomp)
-    end
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  # get the first name
+  name = gets .chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+  # add the student hash to the array
+  @students << {name: name, cohort: :november}
+  puts "Now we have #{@students.count} students"
+  # get another name from the user
+  name = gets.chomp
   end
-  students
+  # return the array of students
+  @students
 end
-
-students=input_students
 
 def print_header
- puts "The students of Villains Academy"
- puts "-------------".center("The students of Villains Academy".length)
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-=begin
-def print_names(students,first_l)
- students.each_with_index do |student,i|
-   if student[:name][0]==first_l &&student[:name].length<12
-     puts "#{i+1}. #{ student[:name]}, (#{student[:cohort]} cohort)"
-   end
-  end
-end
-=end
-def cohort_list(students)
-  ary=[]
-  students.each{|a| ary<<students[a][:cohort] }
-  ary
-end
-
-#with a while loop - until would be until ==students.length, otherwise same.
-def print_names(students,cohortname)
-  i=0;
-  while i<students.length do
-   if students[i][:cohort]==cohortname.intern &&students[i][:name].length<12
-     #print out student name, cohort,hobbies and birthday; then center it
-     puts "#{i+1}. #{ students[i][:name]}, (#{students[i][:cohort]} cohort, likes #{students[i][:hobbies]}, born #{students[i][:birthday]})".center(80,'*')
-   end
-   i+=1
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)\n"
   end
 end
 
-def print_footer(students)
-  print "Overall, we have #{students.length} great #{students.length==1 ? "student" : "students"}"
+def print_footer
+  puts "Overall, we have #{@students.length} great students"
 end
-print_header
 
-puts "What cohort are they in?"
-print_names(students,gets.chomp)
-print_footer(students)
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_file
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+
+def save_file
+  file=File.open("students.csv","w")
+  @students.each do |a|
+    student_data=[a[:name],a:[:cohort]]
+    #sep with commas
+    csv_value=student_data.join(",")
+    #write to file line by line
+    file.puts csv_value
+  end
+  file.close
+end
+
+interactive_menu
